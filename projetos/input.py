2161df6ae5096c82c760f82c3ffc26213eaa6e17
Andreazo Silva <andreazocoisas@gmail.com>
2024-08-26 08:51:27 -0300

    add more tasks

---
+++ projetos/input.py
@@ -0,0 +1,4 @@
+#! /usr/bin/python3
+from math import pi
+raio = input("Digite o valor do raio do circulo: ")
+print(f"Área do Circulo eh: {pi * float(raio) ** 2}")
---
+++ projetos/operacoes_matematicas.py
@@ -0,0 +1,4 @@
+#! /usr/bin/python3
+from math import pi
+
+print(f"valor de PI: {pi}")
